0db30fab7052eb8e4ad5db4f896983af365c1608
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-15 12:12:45 +0100

    Created default route

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from school.views import student
+from django.urls import path, include
+from school.views import StudentsViewSet, CoursesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('students', StudentsViewSet, basename='Students')
+router.register('Courses', CoursesViewSet, basename='Courses')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/', student),
+    path('', include(router.urls)),
 ]
