f9970795e7da5f1cf689eab1ba5d0ed1483954d5
bonelli95 <rodrigobonelli95@gmail.com>
2024-04-21 07:16:08 +0100

    Configured Django Rest CORS/Integrated Django Rest API with a React application

--- school/views.py
+++ school/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
 from school.models import Student, Course, Enrollment
 from school.serializer import StudentSerializer, StudentSerializerV2, CourseSerializer, EnrollmentSerializer, ListEnrollmentStudentSerializer, ListStudentsEnrollmentSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 class StudentsViewSet(viewsets.ModelViewSet):
     """displaying all students"""
@@ -15,11 +17,22 @@ class CoursesViewSet(viewsets.ModelViewSet):
     """displaying all courses"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    
+    def create(self, request):
+        """self-documenting protocol"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
 
 class EnrollmentsViewSet(viewsets.ModelViewSet):
     """displaying all enrollment"""
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
+    #http_method_names =['get', 'post', 'put', 'path', 'delete']
 
 class ListEnrollmentStudent(generics.ListAPIView):
     """displaying all students enrollment"""
--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'school',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,16 +123,26 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.DjangoModelPermissions'
+    #'DEFAULT_PERMISSION_CLASSES':[
+    #    'rest_framework.permissions.IsAuthenticated',
+    #    'rest_framework.permissions.DjangoModelPermissions'
+    #],
+    #'DEFAULT_AUTHENTICATION_CLASSES':[
+    #    'rest_framework.authentication.BasicAuthentication',
+    #],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.BasicAuthentication',
-    ]
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+    }
 }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
